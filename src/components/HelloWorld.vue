48fe7f35989903cd0a1199e2d6a0d4063f77dde7
Juandi Vallejo <jdvallejo1696@gmail.com>
2025-11-21 06:08:48 -0500

    Successful Supabase connection

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -9,7 +9,7 @@
 
       <div class="mb-8 text-center">
         <div class="text-body-2 font-weight-light mb-n1">Welcome to</div>
-        <h1 class="text-h2 font-weight-bold">Vuetify</h1>
+        <h1 class="text-h2 font-weight-bold">PBJ FILMS (v2)</h1>
       </div>
 
       <v-row>
@@ -39,52 +39,27 @@
             </template>
           </v-card>
         </v-col>
-
-        <v-col v-for="link in links" :key="link.href" cols="6">
-          <v-card
-            append-icon="mdi-open-in-new"
-            class="py-4"
-            color="surface-variant"
-            :href="link.href"
-            :prepend-icon="link.icon"
-            rel="noopener noreferrer"
-            rounded="lg"
-            :subtitle="link.subtitle"
-            target="_blank"
-            :title="link.title"
-            variant="tonal"
-          />
+        <v-col>
+          <ul>
+            <li v-for="movie in movies" :key="movie.tmdb_id">{{ movie.title }}</li>
+          </ul>
         </v-col>
       </v-row>
+
     </div>
   </v-container>
 </template>
 
 <script setup>
-  const links = [
-    {
-      href: 'https://vuetifyjs.com/',
-      icon: 'mdi-text-box-outline',
-      subtitle: 'Learn about all things Vuetify in our documentation.',
-      title: 'Documentation',
-    },
-    {
-      href: 'https://vuetifyjs.com/introduction/why-vuetify/#feature-guides',
-      icon: 'mdi-star-circle-outline',
-      subtitle: 'Explore available framework Features.',
-      title: 'Features',
-    },
-    {
-      href: 'https://vuetifyjs.com/components/all',
-      icon: 'mdi-widgets-outline',
-      subtitle: 'Discover components in the API Explorer.',
-      title: 'Components',
-    },
-    {
-      href: 'https://discord.vuetifyjs.com',
-      icon: 'mdi-account-group-outline',
-      subtitle: 'Connect with Vuetify developers.',
-      title: 'Community',
-    },
-  ]
+  import { supabase } from '../utils/supabase'
+  const movies = ref([])
+
+  async function getMovies () {
+    const { data } = await supabase.from('pbj-movies').select()
+    movies.value = data
+  }
+
+  onMounted(() => {
+    getMovies()
+  })
 </script>
