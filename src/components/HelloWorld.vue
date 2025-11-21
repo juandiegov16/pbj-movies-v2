a64528a9fbf4359e8ca15f2c39351121c1c4fe68
Juandi Vallejo <jdvallejo1696@gmail.com>
2025-11-21 06:24:42 -0500

    Vercel conn + basic movie fetch

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -13,32 +13,6 @@
       </div>
 
       <v-row>
-        <v-col cols="12">
-          <v-card
-            class="py-4"
-            color="surface-variant"
-            image="https://cdn.vuetifyjs.com/docs/images/one/create/feature.png"
-            prepend-icon="mdi-rocket-launch-outline"
-            rounded="lg"
-            variant="tonal"
-          >
-            <template #image>
-              <v-img position="top right" />
-            </template>
-
-            <template #title>
-              <h2 class="text-h5 font-weight-bold">
-                Get started
-              </h2>
-            </template>
-
-            <template #subtitle>
-              <div class="text-subtitle-1">
-                Change this page by updating <v-kbd>{{ `<HelloWorld />` }}</v-kbd> in <v-kbd>components/HelloWorld.vue</v-kbd>.
-              </div>
-            </template>
-          </v-card>
-        </v-col>
         <v-col>
           <ul>
             <li v-for="movie in movies" :key="movie.tmdb_id">{{ movie.title }}</li>
