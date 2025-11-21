b0e1b25a47bb247eb2046195c0c765f75bbd07c2
Juandi Vallejo <jdvallejo1696@gmail.com>
2025-11-21 16:28:28 -0500

    alignment fixes + card layout

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,31 @@
 <template>
-  <v-container class="fill-height" max-width="900">
-    <div>
-      <v-img class="mb-4" height="150" src="@/assets/logo.png" />
+  <v-container class="fill-height max-width">
+    <div class="mb-8 text-center justify-center d-flex">
+      <h1 class="text-h4 font-weight-bold">PBJ FILMS (v2)</h1>
+    </div>
+    <div class="d-flex justify-center" :items-per-page="itemsPerPage" :page="page">
+      <!-- <v-img class="mb-4" height="150" src="@/assets/logo.png" /> -->
 
-      <div class="mb-8 text-center">
-        <div class="text-body-2 font-weight-light mb-n1">Welcome to</div>
-        <h1 class="text-h2 font-weight-bold">PBJ FILMS (v2)</h1>
-      </div>
-
-      <!-- <v-row>
-        <v-col v-for="movie in movies" :key="movie.tmdb_id" cols="3">
-          <v-card class="mx-auto" rounded="lg" :title="movie.title">
-            <v-divider />
-            <v-card-text>
-              <span><i><b>Director:</b> {{movie.credits.crew.find(person => person.job === "Director").name}}</i>
-                <br></span>
-              <v-badge bordered color="success" :content="movie.date_watched" inline location="top right">
-                <v-icon icon="mdi-calendar" />
-              </v-badge>
-              <v-divider />
-              <ul>
-                <li v-for="person in movie.credits.cast.slice(0, 9)" :key="person.id">
-                  {{ person.name }} <span class="text-gray-500">as</span> {{ person.character }}
-                </li>
-              </ul>
-            </v-card-text>
-
-          </v-card>
-        </v-col>
-      </v-row> -->
-
-      <v-timeline v-for="movie in movies" :key="movie.tmdb_id">
-        <v-timeline-item align="center" class="mb-4">
+      <v-timeline v-for="movie in movies" :key="movie.tmdb_id" align="center" direction="horizontal">
+        <v-timeline-item class="md-4">
           <template #opposite>
-            <div class="text-h5" v-text="movie.title" />
-            <v-badge
-              bordered
-              color="success"
-              :content="movie.date_watched"
-              inline
-              location="top right"
-            >
-              <v-icon icon="mdi-calendar" />
-            </v-badge><div>
-              <p class="text-button">Director:</p> <i> {{ movie.credits.crew.find(person => person.job === "Director").name }}</i>
-            </div>
-          </template>
-          <!-- <ul class="text-body-1">
-            <li v-for="person in movie.credits.cast.slice(0, 9)" :key="person.id" class="text-body-1 text-left">
-              {{ person.name }} <span class="text-gray-500">as</span> {{ person.character }}
-            </li>
-          </ul> -->
+            <v-card class="mx-auto" :title="movie.title" align="center">
+              <div class="d-flex ga-12 justify-center mb-2"><v-badge
+                bordered
+                color="success"
+                :content="movie.date_watched"
+                inline
+                location="right center"
+              >
+                <v-icon icon="mdi-calendar" />
+              </v-badge></div><v-card-subtitle>{{ movie.credits.crew.find(person => person.job ===
+                "Director").name }}</v-card-subtitle>
 
-          <v-card class="mx-auto" max-width="300">
+            </v-card>
+
+          </template>
+
+          <v-card class="mx-auto">
             <v-list-item
               v-for="person in movie.credits.cast.slice(0, 9)"
               :key="person.id"
@@ -61,7 +34,11 @@
             />
           </v-card>
         </v-timeline-item>
-
+        <template #bottom>
+          <div class="text-center pt-2">
+            <v-pagination v-model="page" :length="pageCount" :total-visible="7" />
+          </div>
+        </template>
       </v-timeline>
 
     </div>
@@ -71,9 +48,15 @@
 <script setup>
   import { supabase } from '../utils/supabase'
   const movies = ref([])
+  const page = ref(1)
+  const itemsPerPage = ref(5)
+
+  const pageCount = computed(() => {
+    return Math.ceil(movies.value.length / itemsPerPage.value)
+  })
 
   async function getMovies () {
-    const { data } = await supabase.from('pbj-movies').select().order('date_watched', { ascending: false })
+    const { data } = await supabase.from('pbj-movies').select().order('date_watched', { ascending: false }).range(0, 4)
     movies.value = data
   }
 
